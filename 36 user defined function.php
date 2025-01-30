83a6ae5c7ac2aa91ca41db7319e037678f7c84e2
ArnavKharbanda <arnavkharbanda2910@gmail.com>
2025-01-30 12:00:58 +0530

    Add files via upload

---
+++ 36 user defined function.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title> Writing PHP function </title>
+
+</head>
+<body>
+
+<?php
+    function writeMessage()
+    {
+        echo "You are really a nice person, Have a nice time!";
+    }
+   
+    writeMessage();
+echo "<br>";
+echo "Program by Arnav Kharbanda,2220100249"
+    
+?>
+
+</body>
+</html>
